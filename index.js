3971974459d6687342d67760d70c9453f29af978
github username <github email address>
2016-11-03 22:14:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  window.kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  window.kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(array){
+  window.kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(array){
+  window.kittens.shift();
+}
+
+function appendKitten(name){
+  var array = [...window.kittens,name];
+  return array;
+}
+
+function prependKitten(name){
+  var array = [name,...window.kittens];
+  return array;
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
